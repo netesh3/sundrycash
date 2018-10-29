c17636f1324d19c4b2d1735384ca4b51e421fe3a
Netesh Kumar <netesh91@gmail.com>
2018-10-30 00:09:44 +0530

    first commit

---
+++ serviceimpl/src/main/java/com/code/serviceimpl/ServiceimplApplication.java
@@ -0,0 +1,12 @@
+package com.code.serviceimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceimplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceimplApplication.class, args);
+	}
+}
---
+++ src/main/java/com/code/sundrycash/SundrycashApplication.java
@@ -0,0 +1,12 @@
+package com.code.sundrycash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundrycashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundrycashApplication.class, args);
+	}
+}
